444db3fab454e4d3cf9f27181bd0ed40d08d81ae
Mateusz Moniowski <mlynski123@gmail.com>
2023-10-23 23:47:46 +0200

    Add cars

---
+++ Labolatorium3 - App/Controllers/CarController.cs
@@ -0,0 +1,76 @@
+﻿using Labolatorium3___App.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Labolatorium3___App.Controllers
+{
+    public class CarController : Controller
+    {
+        static readonly Dictionary<int, Car> _cars = new Dictionary<int, Car>();
+        static int id = 1;
+        public IActionResult Index()
+        {
+            return View(_cars);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Car model)
+        {
+            if(ModelState.IsValid)
+            {
+                model.Id = id++;
+                _cars[model.Id] = model;
+                //zapisanie modelu
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            return View(_cars[id]);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Car model)
+        {
+            if (ModelState.IsValid)
+            {
+                _cars[model.Id] = model;
+                //zapisanie modelu
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            return View(_cars[id]);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Car model)
+        {
+            _cars.Remove(model.Id);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            return View(_cars[id]);
+        }
+
+
+    }
+}
---
+++ Labolatorium3 - App/Models/Car.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Labolatorium3___App.Models
+{
+    public class Car
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Musisz podać markę")]
+        [StringLength(maximumLength: 50, ErrorMessage = "Zbyt długa nazwa marki (maksymalnie 50 znaków)")]
+        public string Maker { get; set; }
+
+
+        [Required(ErrorMessage = "Musisz podać model")]
+        [StringLength(maximumLength: 50, ErrorMessage = "Zbyt długa nazwa modelu (maksymalnie 50 znaków)")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Musisz podać pojemność silnika")]
+        [Range(0, int.MaxValue, ErrorMessage = "Pojemność silnika musi być większa od zera")]
+        public int Volume { get; set; }
+
+
+        [Required(ErrorMessage = "Musisz podać moc silnika")]
+        [Range(0, int.MaxValue, ErrorMessage = "Moc silnika musi być większa od zera")]
+        public int Power { get; set; }
+
+        [Required(ErrorMessage = "Musisz podać typ")]
+        [StringLength(maximumLength: 50, ErrorMessage = "Zbyt długa nazwa typu (maksymalnie 50 znaków)")]
+        public string Type { get; set; }
+
+        [Required(ErrorMessage = "Musisz podać typ")]
+        [StringLength(maximumLength: 10, ErrorMessage = "Zbyt długi numer rejestracyjny (maksymalnie 10 znaków)")]
+        public string Registration { get; set; }
+
+        public string? Owner { get; set; }
+    }
+}
